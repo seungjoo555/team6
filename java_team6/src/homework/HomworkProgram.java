461ca3f1dbd54895d3acca971559a3a2b2b215de
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-01-03 14:13:56 +0900

    팀과제

---
+++ java_team6/src/homework/HomworkProgram.java
@@ -0,0 +1,25 @@
+package homework;
+
+import program.Program;
+
+public class HomworkProgram implements Program {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void runMenu(int menu) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
