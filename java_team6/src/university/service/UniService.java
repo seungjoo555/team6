7a14e952cce563a3e38d2de8243d3770a3558905
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-01-18 17:48:46 +0900

    12

--- java_team6/src/university/service/UniService.java
+++ java_team6/src/university/service/UniService.java
@@ -1,6 +1,55 @@
 package university.service;
 
+import java.util.List;
+
+import acountbook.Item;
+
 // 서비스 인터페이스
 public interface UniService {
+	
+		//전체 조회 : 이승주
+		void printAll(List<Item> list);
+		//월별 조회 : 이승주
+		void printMonth(List<Item> list);
+		//날짜별 조회 : 이승주
+		void printDay(List<Item> list);
+		//수입 품목을 추가하는 메서드 : 이철범
+		List<Item> add(List<Item> list);
+		//지출 품목을 수정하는 메서드 : 이철범
+		List<Item> updateSpending(List<Item> list, int index);
+		// 수입 삭제 : 임병훈
+		List<Item> delete(List<Item> list, int index);
+		// 수입 수정 : 임병훈
+		List<Item> update(List<Item> list, int index);
+		// 원하는 내역 index값 찾기 : 임병훈
+		int location(List<Item> list);
+
+		// 교수 추가 : 정경호
+		boolean Professor(List<Item> list);
+		// 교수 삭제 : 정경호
+		boolean removeProfessor(List<Item> list);
+		// 교수 수정 : 정경호
+		boolean updateProfessor(List<Item> list);//=
+		
+		// 학생 추가 : 정경호
+		boolean Student(List<Item> list);
+		// 학생 삭제 : 정경호
+		boolean removeStudent(List<Item> list);
+		// 학생 수정 : 정경호
+		boolean updateStudent(List<Item> list);//=
+		
+		// 강의 추가 : 정경호
+		boolean Subject(List<Item> list);
+		// 강의 삭제 : 정경호
+		boolean removeSubject(List<Item> list);
+		// 강의 수정 : 정경호
+		boolean updateSubject(List<Item> list);//=
+		
+		// 학과 추가 : 정경호
+		boolean Department(List<Item> list);
+		// 학과 삭제 : 정경호
+		boolean removeDepartment(List<Item> list);
+		// 학과 수정 : 정경호
+		boolean updateDepartment(List<Item> list);//=
 
 }
--- java_team6/src/university/service/UniServiceimp.java
+++ java_team6/src/university/service/UniServiceimp.java
@@ -1,6 +1,130 @@
 package university.service;
 
+import java.util.List;
+
+import acountbook.Item;
+
 // 서비스 구현클래스
 public class UniServiceimp implements UniService {
+
+	@Override
+	public void printAll(List<Item> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printMonth(List<Item> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printDay(List<Item> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Item> add(List<Item> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> updateSpending(List<Item> list, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> delete(List<Item> list, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> update(List<Item> list, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int location(List<Item> list) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean Professor(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeProfessor(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateProfessor(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Student(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeStudent(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateStudent(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Subject(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeSubject(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateSubject(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Department(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeDepartment(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateDepartment(List<Item> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
