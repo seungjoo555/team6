aa3a8a356e41c61e2d6f1259da72c215ed7fa9ce
LCB <dhtkfkdh2073@naver.com>
2024-01-22 09:16:05 +0900

    가계부

--- java_team6/src/acountbook/Item.java
+++ java_team6/src/acountbook/Item.java
@@ -5,8 +5,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Objects;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
@@ -17,50 +15,14 @@ import lombok.NonNull;
 public class Item implements Serializable{
 	
 	private static final long serialVersionUID = -7504275902981826903L;
+
 	private int year, month, day;	//작성일
 	private String title;			//품목
 	private int money;				//수입,지출
-	private int num;				//가계부 고유번호
 	@NonNull
 	private Date regDate;
 	
-	//수정 또는 삭제할때 비교
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Item other = (Item) obj;
-		return num == other.num && Objects.equals(regDate, other.regDate) && Objects.equals(title, other.title);
-	}
 	
-	@Override
-	public int hashCode() {
-		return Objects.hash(num, regDate, title);
-	}
-	
-	//수정 또는 삭제할때 사용할 생성자
-	public Item(String str, String title, int num) {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			this.regDate = format.parse(str);
-			this.title = title;
-			Calendar cal = Calendar.getInstance();
-			cal.setTime(regDate);
-			this.year = cal.get(Calendar.YEAR);
-			this.month = cal.get(Calendar.MONTH) + 1;
-			this.day = cal.get(Calendar.DAY_OF_MONTH);
-			this.money = 0;
-			this.num = num;
-		} catch (ParseException e) {
-			System.out.println("에러");
-		}
-	}
-	
-	//추가할때 사용할 생성자
 	public Item(String str, String title) {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		try {
@@ -76,7 +38,8 @@ public class Item implements Serializable{
 			System.out.println("에러");
 		}
 	}
-	
+	/* 강사 피드백
+	 * - money가 음수일 때 처리 안함 아래도 마찬가지*/
 	public void incomeMoney(int money) {
 		this.money += money;
 	}
@@ -91,5 +54,11 @@ public class Item implements Serializable{
 		return format.format(regDate) + "\t" + title + "\t" + money;
 	}
 
-	
+	public Item(int year, int month, int day, int money, String title) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.money = money;
+		this.title = title;
+	}
 }
