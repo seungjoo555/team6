554a5bc411b3535e5ce7f3d200c0500236b0d83e
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-01-28 21:20:54 +0900

    가으이

--- java_team6/src/university/UniProgram.java
+++ java_team6/src/university/UniProgram.java
@@ -1,13 +1,17 @@
 package university;
 
 import java.util.InputMismatchException;
+
 import java.util.Scanner;
+
+import lombok.Data;
 import program.UniversityProgram;
 import university.service.PrintService;
 import university.service.PrintServiceImp;
 import university.service.UniService;
 import university.service.UniServiceImp;
 
+@Data
 // 실행 메서드
 public class UniProgram implements UniversityProgram {
 	
@@ -16,7 +20,7 @@ public class UniProgram implements UniversityProgram {
 	private School school = new School();
 	
 	
-	private Scanner sc = new Scanner(System.in);
+	public static Scanner scan = new Scanner(System.in);
 	private final int EXIT = 7;
 	private final int PFMEXIT = 4;
 	private final int STDMEXIT = 4;
@@ -34,12 +38,12 @@ public class UniProgram implements UniversityProgram {
 			ps.printMainMenu();
 			try {
 				//메뉴 선택
-				menu = sc.nextInt();
+				menu = scan.nextInt();
 				//메뉴 실행
 				runMenu(menu);
 			} catch (InputMismatchException e) {
 				System.out.println("메뉴를 잘못 선택했습니다.");
-				sc.nextLine();
+				scan.nextLine();
 			}
 		}while(menu != EXIT);
 		//저장
@@ -89,7 +93,7 @@ public class UniProgram implements UniversityProgram {
 			//메뉴 출력
 			ps.printMenu();
 			// 메뉴 선택
-			menu = sc.nextInt();
+			menu = scan.nextInt();
 			//메뉴 실행
 			runSearch(menu);
 		}while(menu != SEARCHEXIT);
@@ -132,7 +136,7 @@ public class UniProgram implements UniversityProgram {
 			//메뉴 출력
 			ps.printPFMMenu();
 			//메뉴 선택
-			menu = sc.nextInt();
+			menu = scan.nextInt();
 			//메뉴 실행
 			runPFMMenu(menu);
 		}while(menu != PFMEXIT);
@@ -171,7 +175,7 @@ public class UniProgram implements UniversityProgram {
 			//메뉴 출력
 			ps.printSTDMMenu();
 			//메뉴 선택
-			menu = sc.nextInt();
+			menu = scan.nextInt();
 			//메뉴 실행
 			runSTDMMenu(menu);
 		}while(menu != STDMEXIT);
@@ -210,7 +214,7 @@ public class UniProgram implements UniversityProgram {
 			//메뉴 출력
 			ps.printDPMMenu();
 			//메뉴 선택
-			menu = sc.nextInt();
+			menu = scan.nextInt();
 			//메뉴 실행
 			runDPMMenu(menu);
 		}while(menu != DPMEXIT);
@@ -246,9 +250,11 @@ public class UniProgram implements UniversityProgram {
 			//메뉴 출력
 			ps.printSJMMenu();
 			//메뉴 선택
-			menu = sc.nextInt();
+			menu = scan.nextInt();
 			//메뉴 실행
+			
 			runSJMMenu(menu);
+			
 		}while(menu != SJMEXIT);
 	}
 
