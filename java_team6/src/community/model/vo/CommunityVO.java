5f3fb3a36471130f45d9653a4548ed262214a8f1
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-02-13 17:52:35 +0900

    게시판 구현중

--- java_team6/src/community/model/vo/CommunityVO.java
+++ java_team6/src/community/model/vo/CommunityVO.java
@@ -6,7 +6,6 @@ public class CommunityVO {
 	private String id;
 	private String pw;
 	private String email;
-	private Date reg_Date;
 	
 	public CommunityVO(String id, String pw, String email) {
 		this.id = id;
