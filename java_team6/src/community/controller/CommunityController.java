85b270a30b7a8f521940ea6eda11eb04fea3d830
LCB <dhtkfkdh2073@naver.com>
2024-02-14 16:37:05 +0900

    틀

--- java_team6/src/community/controller/CommunityController.java
+++ java_team6/src/community/controller/CommunityController.java
@@ -31,16 +31,16 @@ public class CommunityController {
 	private void runMenu(int menu) {
 		switch(menu) {
 		case 1:
-			MemberMenu();
+			memberMenu();
 			break;
 		case 2:
-			BoardMenu();
+			boardMenu();
 			break;
 		case 3:
-			PostMenu();
+			postMenu();
 			break;
 		case 4:
-			CommentMenu();
+			commentMenu();
 			break;
 		case 5:
 			System.out.println("이전 메뉴로 돌아갑니다.");
@@ -51,20 +51,212 @@ public class CommunityController {
 		
 	}
 
+	private void commentMenu() {
+		int menu;
+		do {
+			CommentMenu();
+			menu = scan.nextInt();
+			runCommentMenu(menu);
+		}while(menu != 4);
+	}
+
 	private void CommentMenu() {
+		System.out.println("메뉴");
+		System.out.println("1. 댓글 등록");
+		System.out.println("2. 게시글 수정");
+		System.out.println("3. 댓글 삭제");
+		System.out.println("4. 이전으로");
+		System.out.print("메뉴 선택 : ");	
+	}
+
+	private void runCommentMenu(int menu) {
+		switch(menu) {
+		case 1:
+			insertComment();
+			break;
+		case 2:
+			updateComment();
+			break;
+		case 3:
+			deleteComment();
+			break;
+		case 4:
+			System.out.println("이전 메뉴로 돌아갑니다.");
+			break;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	private void insertComment() {
+		// TODO Auto-generated method stub
 		
 	}
 
-	private void PostMenu() {
+	private void updateComment() {
+		// TODO Auto-generated method stub
 		
 	}
 
-	private void BoardMenu() {
+	private void deleteComment() {
+		// TODO Auto-generated method stub
 		
 	}
 
-	private void MemberMenu() {
+	private void postMenu() {
+		int menu;
+		do {
+			printPostMenu();
+			menu = scan.nextInt();
+			runPostMenu(menu);
+		}while(menu != 4);
+	}
+
+	private void printPostMenu() {
+		System.out.println("메뉴");
+		System.out.println("1. 게시글 등록");
+		System.out.println("2. 게시글 수정");
+		System.out.println("3. 게시글 삭제");
+		System.out.println("4. 이전으로");
+		System.out.print("메뉴 선택 : ");	
+	}
+
+	private void runPostMenu(int menu) {
+		switch(menu) {
+		case 1:
+			insertPost();
+			break;
+		case 2:
+			updatePost();
+			break;
+		case 3:
+			deletePost();
+			break;
+		case 4:
+			System.out.println("이전 메뉴로 돌아갑니다.");
+			break;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	private void insertPost() {
+		// TODO Auto-generated method stub
 		
+	}
+
+	private void updatePost() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void deletePost() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void boardMenu() {
+		int menu;
+		do {
+			printBoardMenu();
+			menu = scan.nextInt();
+			runBoardMenu(menu);
+		}while(menu != 4);
+	}
+
+	private void runBoardMenu(int menu) {
+		switch(menu) {
+		case 1:
+			insertBoard();
+			break;
+		case 2:
+			updateBoard();
+			break;
+		case 3:
+			deleteBoard();
+			break;
+		case 4:
+			System.out.println("이전 메뉴로 돌아갑니다.");
+			break;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	private void insertBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void updateBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void deleteBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void printBoardMenu() {
+		System.out.println("메뉴");
+		System.out.println("1. 게시판 등록");
+		System.out.println("2. 게시판 수정");
+		System.out.println("3. 게시판 삭제");
+		System.out.println("4. 이전으로");
+		System.out.print("메뉴 선택 : ");	
+	}
+
+	private void memberMenu() {
+		int menu;
+		do {
+			printMemberMenu();
+			menu = scan.nextInt();
+			runMemeberMenu(menu);
+		}while(menu != 4);
+	}
+
+	private void runMemeberMenu(int menu) {
+		switch(menu) {
+		case 1:
+			insertMember();
+			break;
+		case 2:
+			login();
+			break;
+		case 3:
+			deleteMember();
+			break;
+		case 4:
+			System.out.println("이전 메뉴로 돌아갑니다.");
+			break;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	private void insertMember() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void deleteMember() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void printMemberMenu() {
+		System.out.println("메뉴");
+		System.out.println("1. 회원 가입");
+		System.out.println("2. 로그인");
+		System.out.println("3. 회원 탈퇴");
+		System.out.println("4. 이전으로");
+		System.out.print("메뉴 선택 : ");
 	}
 
 	private void printMenu() {
@@ -75,7 +267,6 @@ public class CommunityController {
 		System.out.println("4. 댓글 관리");
 		System.out.println("5. 이전으로");
 		System.out.print("메뉴 선택 : ");
-		
 	}
 
 }
