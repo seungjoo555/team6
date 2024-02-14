bac758f9e4c0d48e34f918e8e8d4bc609bdfa4b7
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-02-14 18:42:53 +0900

    3

--- java_team6/src/db/community/mybatis/main/Main.java
+++
@@ -1,7 +0,0 @@
-package db.community.mybatis.main;
-
-public class Main {
-	public static void main(String[] args) {
-		
-	}
-}
--- java_team6/src/db/community/mybatis/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package db.community.mybatis.service;
-
-public interface BoardService {
-	void addBoard();
-	void setBoard();
-	void delBoard();
-	void printBoard();
-
-}
