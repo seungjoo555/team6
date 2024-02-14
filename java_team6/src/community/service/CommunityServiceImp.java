9078a8e284bf913a705975b90364a773ac35b910
LCB <dhtkfkdh2073@naver.com>
2024-02-14 17:42:01 +0900

    틀

--- java_team6/src/community/service/CommunityServiceImp.java
+++ java_team6/src/community/service/CommunityServiceImp.java
@@ -1,5 +1,31 @@
 package community.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import community.dao.CommunityDAO;
+
 public class CommunityServiceImp implements CommunityService {
+	
+	private CommunityDAO communityDao;
+	
+	public CommunityServiceImp() {
+		String resource = "community/config/mybatis-config.xml";
+		InputStream inputStream;
+		SqlSession session;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			session = sessionFactory.openSession(true);
+			communityDao = session.getMapper(CommunityDAO.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
