e1a806a5de4e475690a4623b1c9ff7a3a253d54e
LCB <dhtkfkdh2073@naver.com>
2024-02-19 21:30:27 +0900

    123

--- java_team6/src/community/controller/CommunityController.java
+++ java_team6/src/community/controller/CommunityController.java
@@ -346,7 +346,7 @@ public class CommunityController {
 			try {
 				System.out.println("메뉴");
 				System.out.println("1. 게시글 삭제");
-				System.out.println("2. 게시글 삭제");
+				System.out.println("2. 게시글 조회");
 				System.out.println("0. 이전으로");
 				System.out.print("메뉴 선택 : ");
 				menu = scan.nextInt();
